51b37acd7b4fada4219af8d794b154ca8576526a
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2021-11-28 16:13:57 +0100

    Use json to pass songs objects list from django database to javascript file

--- player/static/player/player.js
+++ player/static/player/player.js
@@ -3,33 +3,9 @@ let isPlaying = false;
 let timer;
 let songAudio = document.createElement('audio');
 
-const songObjsList = createObjList();
+const songObjsList = JSON.parse(document.getElementById('songObjsList').textContent);
 
 updateActiveSong(songId)
-
-
-// Function that creates list with song objects from django database
-// songsObjsString variable is created in <head> section, in template
-// and contains all data in one string: "[{...},{...},{...}]"
-function createObjList() {
-  const listWithObjsString = songObjsString;
-
-  // Create list with strings containing objects: ["{...}","{...}","{...}"]
-  let objsString = listWithObjsString.substring(listWithObjsString.indexOf("[") + 1, listWithObjsString.indexOf("]"));
-  let objsArray = objsString.split("}, ");
-  for (i = 0; i < objsArray.length - 1; i++) {
-    objsArray[i] += "}";
-  };
-
-  // Change strings into objects
-  let songObjs = [];
-  for (i = 0; i < objsArray.length; i++) {
-    songObjs.push(JSON.parse(objsArray[i].replace(/'/g, '"')))
-  };
-
-  return songObjs;
-};
-
 
 // Load audio file to 'songAudio' variable
 function loadAudio(songId) {
